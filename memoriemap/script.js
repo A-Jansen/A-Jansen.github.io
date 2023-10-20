069ab6446590c418b08305cd220f22f91f77f095
Jansen <anniekjee@hotmail.com>
2023-10-20 12:15:49 +0200

    oocsi

---
+++ memoriemap/script.js
@@ -0,0 +1,21 @@
+OOCSI.connect('wss://oocsi.id.tue.nl/ws')
+OOCSI.subscribe("cultureMap_interface", function (msg) {
+    // handle message on “test channel"
+    // var selectedCity = msg.data.city;
+    if (msg.data.location == "Eindhoven") {
+        console.log("Eindhoven")
+        window.location.href = "page-eindhoven.html";
+    }
+    if (msg.data.location == "Groningen") {
+        console.log("Groningen")
+        window.location.href = "page-groningen.html";
+    }
+    if (msg.data.location == "Enschede") {
+        console.log("Enschede")
+        window.location.href = "page-enschede.html";
+    }
+    if (msg.data.location == "Amsterdam") {
+        console.log("Amsterdam")
+        window.location.href = "page-amsterdam.html";
+    }
+});
